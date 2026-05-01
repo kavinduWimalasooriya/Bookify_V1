a9c83868523575fba91a1ee78b3da69695721084
Kavindu Wimalasooriya <agkmwimalasooriy@gmail.com>
2026-05-01 14:31:31 +0530

    git init

---
+++ Bookify.Domain/Apartments/Amenity.cs
@@ -0,0 +1,15 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public enum Amenity
+{
+    Wifi = 1,
+    AirConditioner = 2,
+    Parking = 3,
+    PetFriendly = 4,
+    SwimmingPool = 5,
+    Gym = 6,
+    Spa = 7,
+    Terrence = 8,
+    MountainView = 9,
+    GardenView = 10
+}
---
+++ Bookify.Domain/Apartments/Apartment.cs
@@ -0,0 +1,22 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Apartments;
+
+public sealed class Apartment : Entity
+{
+    public Apartment(Guid id) : base(id)
+    {
+    }
+
+    public string? Name { get; private set; }
+    public string? Description { get; private set; }
+    public string Country { get; private set; }
+    public string State { get; private set; }
+    public string City { get; private set; }
+    public string Street { get; private set; }
+    public string ZipCode { get; private set; }
+    public decimal Price { get; private set; }
+    public decimal CleaningFee { get; private set; }
+    public DateTime? LastBookedOnUtc { get; private set; }
+    public List<Amenity> Amemities { get; private set; } = new();
+}
