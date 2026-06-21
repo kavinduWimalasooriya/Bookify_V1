6dace080a8c141b87750651b8036c60841970d00
Kavindu Wimalasooriya <agkmwimalasooriy@gmail.com>
2026-06-21 10:51:28 +0530

    implement validation pipe line

---
+++ Bookify.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,13 @@
+﻿using Bookify.Application.Abstractions.Behaviors;
+
+namespace Bookify.Application.Exceptions;
+
+public class ValidationException : Exception
+{
+    public IEnumerable<ValidationError> Errors { get; }
+    public  ValidationException(IEnumerable<ValidationError> errors)
+    {
+        Errors = errors;
+    }
+    
+}
